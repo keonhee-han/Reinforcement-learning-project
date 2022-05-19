e5780c63e1f3a9ba9c3190583958bf9d2a37bc5f
Keonhee Han <ge96tir@tum.de>
2022-05-19 13:08:03 +0200

    blob detector fixed v1

---
+++ src/tutorial_2/scripts/CV_Alg.py
@@ -0,0 +1,81 @@
+import cv2
+import numpy as np
+
+def blob_detection(self, image):
+    # Transform image into HSV, select parts within the predefined red range color as a mask,
+    # dilate and erode the selected parts to remove noise
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, self.lower_red, self.upper_red)
+    kernel = np.ones((5, 5), np.uint8)
+    mask_dilation = cv2.dilate(mask, kernel, iterations=2)
+    mask_final = cv2.erode(mask_dilation, kernel, iterations=1)
+    kernel = np.ones((6, 6), np.float32) / 25
+    mask_final = cv2.filter2D(mask_final, -1, kernel)
+
+    # Apply mask to original image, show results
+    res = cv2.bitwise_and(image, image, mask=mask_final)
+    cv2.imshow('mask', mask_final)
+    cv2.imshow('image seen through mask', res)
+
+    # Parameter definition for SimpleBlobDetector
+    params = cv2.SimpleBlobDetector_Params()
+    params.filterByArea = True
+    params.minArea = 1000
+    params.maxArea = 200000
+    params.filterByInertia = True
+    params.minInertiaRatio = 0.0
+    params.maxInertiaRatio = 0.8
+
+    # params.filterByConvexity = True
+    # params.minConvexity = 0.09
+    # params.maxConvexity = 0.99
+
+    # Applying the params
+    detector = cv2.SimpleBlobDetector_create(params)
+    keypoints = detector.detect(~mask_final)
+
+    # draw
+    im_with_keypoints = cv2.drawKeypoints(~mask_final, keypoints, np.array([]), (0, 0, 255),
+                                          cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    cv2.imshow("Keypoints", im_with_keypoints)
+
+    ## Find outer contours
+    im, contours, hierarchy = cv2.findContours(mask_final, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+    maxContour = 0
+    for contour in contours:
+        contourSize = cv2.contourArea(contour)
+        if contourSize > maxContour:
+            maxContour = contourSize
+            maxContourData = contour
+
+    ## Draw
+    cv2.drawContours(image, maxContourData, -1, (0, 255, 0), 2, lineType=cv2.LINE_4)
+    cv2.imshow('image with countours', image)
+
+    # Calculate image moments of the detected contour
+    M = cv2.moments(maxContourData)
+
+    try:
+        # Draw a circle based centered at centroid coordinates
+        xPixel = int(M['m10'] / M['m00'])
+        yPixel = int(M['m01'] / M['m00'])
+        cv2.circle(image, ( xPixel, yPixel ), 5, (0, 0, 0), -1)
+        rospy.loginfo("Biggest blob: x Coord: " + str(xPixel) + " y Coord: " + str(yPixel) + " Size: " + str(blobSize))
+
+        # Show image:
+        cv2.imshow("outline contour & centroid", image)
+
+    except ZeroDivisionError:
+        pass
+
+    # Save center coordinates of the blob as a Point() message
+
+    # blob_coordinates_msg = Point(int(M['m10'] / M['m00']), int(M['m01'] / M['m00']), 0)
+
+    # blob_coordinates_msg.x = int(M['m10'] / M['m00'])
+    # blob_coordinates_msg.y = int(M['m01'] / M['m00'])
+    # blob_coordinates_msg.z = 0
+
+    # Publish center coordinates
+    # self.redBlobPub.publish(blob_coordinates_msg)
--- src/tutorial_2/scripts/tutorial_2.py
+++ src/tutorial_2/scripts/tutorial_2.py
@@ -7,6 +7,9 @@ from sensor_msgs.msg import Image,JointState
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
 import numpy as np
+
+import CV_Alg as CVA
+
 ### Keonhee Han
 ### Luisa Mayershofer
 ### Batu Kaan Oezen
@@ -110,86 +113,90 @@ class Central:
 
     def image_cb(self,data):
         bridge_instance = CvBridge()
+
         try:
             br = CvBridge()
- 
+
             # Output debugging information to the terminal
-            rospy.loginfo("receiving video frame")
-            
+            rospy.loginfo("receiving video frame__B_DET")
+
             # Convert ROS Image message to OpenCV image
             current_frame = br.imgmsg_to_cv2(data)
-            current_frame = cv2.cvtColor(current_frame,cv2.COLOR_BGR2HSV)
 
-            params = cv2.SimpleBlobDetector_Params()
+            CVA.blob_detection(current_frame)
 
-            # Change thresholds
-            params.minThreshold = 1
-            params.maxThreshold = 255
-
-
-            # Filter by Area.
-            params.filterByArea = True
-            params.minArea = 30
-
-            # Filter by Circularity
-            params.filterByCircularity = True
-            params.minCircularity = 0.1
-
-            params.filterByColor = True
-            params.blobColor = 255
-
-            
-            lower_green = np.array([160,100,20])
-            upper_green = np.array([179,255,255])
-            # Threshold the HSV image to get only blue colors
-            mask = cv2.inRange(current_frame, lower_green, upper_green)
-            
-            erode_kernel = np.ones((3,3),np.uint8)
-            eroded_img = cv2.erode(mask,erode_kernel,iterations = 1)
-        
-            # dilate
-            dilate_kernel = np.ones((10,10),np.uint8)
-            dilate_img = cv2.dilate(eroded_img,dilate_kernel,iterations = 1)
-            detector = cv2.SimpleBlobDetector_create(params)
-            # Detect blobs.
-            
-            # Create a detector with the parameters
-            # OLD: detector = cv2.SimpleBlobDetector(params)
-            detector = cv2.SimpleBlobDetector_create(params)
-
-
-            # Detect blobs.
-            #keypoints = detector.detect(image_hsv)
-            keypoints = detector.detect(dilate_img)
-            
-            if(len(keypoints) >= 0):
-                max = 0
-                xCoord = 0
-                yCoord = 0
-                maxObject = None
-                for blob in keypoints:
-                    if(blob.size>max):
-                        max = blob.size
-                        xCoord = blob.pt[0]
-                        yCoord = blob.pt[1]
-                        maxObject = blob
-
-            # Round the coordinates to get pixel coordinates:
-            xPixel = round(xCoord)
-            yPixel = round(yCoord)
-
-            # For better readability, round size to 3 decimal indices
-            blobSize = round(max, 3)
-
-            rospy.loginfo("Biggest blob: x Coord: " + str(xPixel) + " y Coord: " + str(yPixel) + " Size: " + str(blobSize))
-
-            #im_with_keypoints = cv2.drawKeypoints(current_frame, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-            cv2.circle(frame,(int(kp_max.pt[0]),int(kp_max.pt[1])),int(kp_max.size),(0,255,0),2)
+            # current_frame = cv2.cvtColor(current_frame,cv2.COLOR_BGR2HSV)
+            #
+            # params = cv2.SimpleBlobDetector_Params()
+            #
+            # # Change thresholds
+            # params.minThreshold = 1
+            # params.maxThreshold = 255
+            #
+            #
+            # # Filter by Area.
+            # params.filterByArea = True
+            # params.minArea = 30
+            #
+            # # Filter by Circularity
+            # params.filterByCircularity = True
+            # params.minCircularity = 0.1
+            #
+            # params.filterByColor = True
+            # params.blobColor = 255
+            #
+            #
+            # lower_green = np.array([160,100,20])
+            # upper_green = np.array([179,255,255])
+            # # Threshold the HSV image to get only blue colors
+            # mask = cv2.inRange(current_frame, lower_green, upper_green)
+            #
+            # erode_kernel = np.ones((3,3),np.uint8)
+            # eroded_img = cv2.erode(mask,erode_kernel,iterations = 1)
+            #
+            # # dilate
+            # dilate_kernel = np.ones((10,10),np.uint8)
+            # dilate_img = cv2.dilate(eroded_img,dilate_kernel,iterations = 1)
+            # detector = cv2.SimpleBlobDetector_create(params)
+            # # Detect blobs.
+            #
+            # # Create a detector with the parameters
+            # # OLD: detector = cv2.SimpleBlobDetector(params)
+            # detector = cv2.SimpleBlobDetector_create(params)
+            #
+            #
+            # # Detect blobs.
+            # #keypoints = detector.detect(image_hsv)
+            # keypoints = detector.detect(dilate_img)
+            #
+            # if(len(keypoints) >= 0):
+            #     max = 0
+            #     xCoord = 0
+            #     yCoord = 0
+            #     maxObject = None
+            #     for blob in keypoints:
+            #         if(blob.size>max):
+            #             max = blob.size
+            #             xCoord = blob.pt[0]
+            #             yCoord = blob.pt[1]
+            #             maxObject = blob
+            #
+            # # Round the coordinates to get pixel coordinates:
+            # xPixel = round(xCoord)
+            # yPixel = round(yCoord)
+            #
+            # # For better readability, round size to 3 decimal indices
+            # blobSize = round(max, 3)
+            #
+            # rospy.loginfo("Biggest blob: x Coord: " + str(xPixel) + " y Coord: " + str(yPixel) + " Size: " + str(blobSize))
+            #
+            # #im_with_keypoints = cv2.drawKeypoints(current_frame, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+            # cv2.circle(frame,(int(kp_max.pt[0]),int(kp_max.pt[1])),int(kp_max.size),(0,255,0),2)
 
             cv2.imshow("Keypoints", im_with_keypoints)
-            
+
             cv2.waitKey(3)
-           
+
         except CvBridgeError as e:
             rospy.logerr(e)
         
@@ -227,8 +234,8 @@ class Central:
         rospy.Subscriber("joint_states",JointState,self.joints_cb)
         rospy.Subscriber("bumper",Bumper,self.bumper_cb)
         rospy.Subscriber("tactile_touch",HeadTouch,self.touch_cb)
-        rospy.Subscriber("/nao_robot/camera/top/camera/image_raw",Image,self.image_cb)
-        self.jointPub = rospy.Publisher("joint_angles",JointAnglesWithSpeed,queue_size=10)
+        rospy.Subscriber("/nao_robot/camera/top/camera/image_raw", Image, self.image_cb)
+        self.jointPub = rospy.Publisher("joint_angles", JointAnglesWithSpeed, queue_size=10)
 
 
         # test sequence to demonstrate setting joint angles
