9b0a43ff619c2fd610a6d9b6f055ab4bc0039415
Luisa <lmayershofer@gmail.com>
2022-05-24 09:03:59 +0200

    combined csv reader + blob detector for recording data, fixed stiffness

--- src/tutorial_3/scripts/tutorial3.py
+++ src/tutorial_3/scripts/tutorial3.py
@@ -6,18 +6,109 @@ from naoqi_bridge_msgs.msg import JointAnglesWithSpeed,Bumper,HeadTouch
 from sensor_msgs.msg import Image,JointState
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
+import numpy as np
+import csv
 
 class tutorial3:
 
     def __init__(self):
         self.blobX = 0
         self.blobY = 0
+        self.blobSize = 0
         self.shoulderRoll = 0
         self.shoulderPitch = 0
-        #self.motionProxy = self.getProxy("ALMotion")
-
         # For setting the stiffnes of single joints
         self.stiffnesPub = 0
+
+    def image_cb(self,data):
+        bridge_instance = CvBridge()
+        try:
+            br = CvBridge()
+ 
+            # Output debugging information to the terminal
+            rospy.loginfo("receiving video frame")
+            
+            # Convert ROS Image message to OpenCV image
+            current_frame = br.imgmsg_to_cv2(data)
+            current_frame = cv2.cvtColor(current_frame,cv2.COLOR_BGR2HSV)
+
+            params = cv2.SimpleBlobDetector_Params()
+
+            # Change thresholds
+            params.minThreshold = 1
+            params.maxThreshold = 255
+
+
+            # Filter by Area.
+            params.filterByArea = True
+            params.minArea = 30
+
+            # Filter by Circularity
+            params.filterByCircularity = True
+            params.minCircularity = 0.1
+
+            params.filterByColor = True
+            params.blobColor = 255
+
+            
+            lower_green = np.array([160,100,20])
+            upper_green = np.array([179,255,255])
+            # Threshold the HSV image to get only blue colors
+            mask = cv2.inRange(current_frame, lower_green, upper_green)
+            
+            erode_kernel = np.ones((3,3),np.uint8)
+            eroded_img = cv2.erode(mask,erode_kernel,iterations = 1)
+        
+            # dilate
+            dilate_kernel = np.ones((10,10),np.uint8)
+            dilate_img = cv2.dilate(eroded_img,dilate_kernel,iterations = 1)
+            detector = cv2.SimpleBlobDetector_create(params)
+            # Detect blobs.
+            
+            # Create a detector with the parameters
+            # OLD: detector = cv2.SimpleBlobDetector(params)
+            detector = cv2.SimpleBlobDetector_create(params)
+
+
+            # Detect blobs.
+            #keypoints = detector.detect(image_hsv)
+            keypoints = detector.detect(dilate_img)
+            max = 0
+            if(len(keypoints) >= 0):
+                max = 0
+                xCoord = 0
+                yCoord = 0
+                maxObject = None
+                for blob in keypoints:
+                    if(blob.size>max):
+                        max = blob.size
+                        xCoord = blob.pt[0]
+                        yCoord = blob.pt[1]
+                        maxObject = blob
+
+            # Round the coordinates to get pixel coordinates:
+            xPixel = round(xCoord)
+            yPixel = round(yCoord)
+
+            
+            self.blobX = xPixel
+            self.blobY = yPixel
+            self.blobSize = max
+
+            # For better readability, round size to 3 decimal indices
+            blobSize = round(max, 3)
+
+            rospy.loginfo("Biggest blob: x Coord: " + str(xPixel) + " y Coord: " + str(yPixel) + " Size: " + str(blobSize))
+
+            im_with_keypoints = cv2.drawKeypoints(current_frame, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+            #cv2.circle(frame,(int(kp_max.pt[0]),int(kp_max.pt[1])),int(kp_max.size),(0,255,0),2)
+
+            cv2.imshow("Keypoints", im_with_keypoints)
+            
+            cv2.waitKey(3)
+           
+        except CvBridgeError as e:
+            rospy.logerr(e)
         
 
     # For reading in touch on TB1
@@ -26,31 +117,30 @@ class tutorial3:
         rospy.loginfo("touch button: "+str(data.button)+" state: "+str(data.state))
         if data.button == 1 and data.state == 1:
             # save those values to a file
-            self.shoulderRoll
-            self.shoulderPitch
-            self.blobX
-            self.blobY
+            # Check if blob has a size greater 0:
+            # Save current variable value to not have it change again due to callback
+            saveX = self.blobX
+            saveY = self.blobY
+            savePitch = self.shoulderPitch
+            saveRoll = self.shoulderRoll
+            size = self.blobSize
+            if size > 0:
+                # open the file in the write mode
+                with open('samples.csv', 'w') as f:
+                    writer = csv.writer(f)
+                    row = [saveX, saveY, savePitch, saveRoll]
+                    writer.writerow(row)
 
 
-    # for setting initial stiffness values
-    def set_stiffness(self,value):
-        if value == True:
-            service_name = '/body_stiffness/enable'
-        elif value == False:
-            service_name = '/body_stiffness/disable'
-        try:
-            stiffness_service = rospy.ServiceProxy(service_name,Empty)
-            stiffness_service()
-        except rospy.ServiceException as e:
-            rospy.logerr(e)
 
     def joints_cb(self,data):
-        for jointNames in data:
-            if jointNames.name == "RShoulderPitch":
-                self.shoulderPitch = jointNames.position
+        for index, jointNames in enumerate(data.joint_names):
+            if jointNames == "RShoulderPitch":
+                self.shoulderPitch = data.joint_angles[index]
+                continue
                 #rospy.loginfo("save")
-            if jointNames.name == "RShoulderRoll":
-                self.shoulderRoll = jointNames.position
+            elif jointNames.name == "RShoulderRoll":
+                self.shoulderRoll = data.joint_angles[index]
             
 
 
@@ -64,23 +154,22 @@ class tutorial3:
 
         self.stiffnesPub = rospy.Publisher("joint_stiffness", JointState, queue_size=10)
 
-
         # Set the initial stiffnesses
         stiffnessState = JointState()
 
-        stiffnessState.msg = "HeadYaw"    #TODO: Head topic 
+        stiffnessState.name = "HeadYaw"
         stiffnessState.effort = 0.9
         self.stiffnesPub.publish(stiffnessState)
 
-        stiffnessState.msg = "HeadPitch"    
+        stiffnessState.name = "HeadPitch"    
         stiffnessState.effort = 0.9
         self.stiffnesPub.publish(stiffnessState)
-
-        stiffnessState.msg = "RShoulderPitch"    
+       
+        stiffnessState.name = "RShoulderPitch"  
         stiffnessState.effort = 0
         self.stiffnesPub.publish(stiffnessState)
 
-        stiffnessState.msg = "RShoulderRoll"    
+        stiffnessState.name = "RShoulderRoll"
         stiffnessState.effort = 0
         self.stiffnesPub.publish(stiffnessState)
 
