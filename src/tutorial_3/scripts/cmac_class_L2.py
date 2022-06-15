e4bf9df1a6480254436be30569f6ae1b60c2495f
TaneliLab <103313689+TaneliLab@users.noreply.github.com>
2022-06-15 08:18:26 +0200

    Update cmac_class_L2.py

--- src/tutorial_3/scripts/cmac_class_L2.py
+++ src/tutorial_3/scripts/cmac_class_L2.py
@@ -11,6 +11,7 @@ DIM_INPUT = 2
 DIM_OUTPUT = 2
 LEARNING_RATE = 0.01
 TRAINING_ITERATIONS = 1000
+TRAINING_SAMPLES = 150
 
 # 315 4 233 4
 # -0.4862360954284668 -1.1059720516204834 0.3141592741012573 -0.3896780014038086
@@ -19,6 +20,7 @@ weights = np.full([2, 50, 50], 0)
 class CMACNetwork:
     def __init__(self):
         self.weights = np.zeros([2, 50, 50])
+        self.weights_index = np.zeros([50, 50])
         self.y1_min = 4
         self.y1_max = 315
         self.y2_min = 4
@@ -28,6 +30,7 @@ class CMACNetwork:
         self.x2_min = -0.4
         self.x2_max = 0.32
         self.MeanError = 0
+        self.step_size = 3  # displacement
 
     def quantization(self, x1, x2, y1, y2):
         x1_q = int((x1 - self.x1_min) * RESOLUTION_RECEPTIVE/(self.x1_max - self.x1_min))
@@ -38,55 +41,79 @@ class CMACNetwork:
         if (x2_q >= RESOLUTION_RECEPTIVE): x2_q = 49
         if (y1_q >= RESOLUTION_RECEPTIVE): y1_q = 49
         if (y2_q >= RESOLUTION_RECEPTIVE): y2_q = 49
+
+        if (x1_q < 0): x1_q = 0
+        if (x2_q < 0): x2_q = 0
+        if (y1_q < 0): y1_q = 0
+        if (y2_q < 0): y2_q = 0
+
         return x1_q, x2_q, y1_q, y2_q
 
-    def localization(self, y1, y2, displacement):
-        # y1, y2 are indexs
-        # output weight location on the map
+    def de_quantization(self, x1_q, x2_q):
+        x1 = x1_q *(self.x1_max - self.x1_min)/RESOLUTION_RECEPTIVE + self.x1_min
+        x2 = x2_q * (self.x2_max - self.x2_min) / RESOLUTION_RECEPTIVE + self.x2_min
+        # y1 = y1_q * (self.y1_max - self.y1_min) / RESOLUTION_RECEPTIVE + self.y1_min
+        # y2 = y2_q * (self.y2_max - self.y2_min) / RESOLUTION_RECEPTIVE + self.y2_min
+        return x1, x2
 
-        location = []
-        if (y1 > RESOLUTION_RECEPTIVE -1 - int((RECEPTIVE_FIELD_SIZE - 1)/2)):
-            start_y1 = RESOLUTION_RECEPTIVE - RECEPTIVE_FIELD_SIZE
-        elif (y1 < int((RECEPTIVE_FIELD_SIZE - 1)/2)):
-            start_y1 = 0
-        else:
-            start_y1 = y1 - (RECEPTIVE_FIELD_SIZE-1)/2
+    def weightindex_init(self):
+        for i in range(0, RESOLUTION_RECEPTIVE):
+            for j in range(0, RESOLUTION_RECEPTIVE):
+                if (i + RESOLUTION_RECEPTIVE*j) % self.step_size == 0:
+                    # print("index:", i, j)
+                    self.weights_index[i][j] = 1
 
-        if (y2 > RESOLUTION_RECEPTIVE -1- int((RECEPTIVE_FIELD_SIZE - 1)/2)):
-            start_y2 = RESOLUTION_RECEPTIVE - RECEPTIVE_FIELD_SIZE
-        elif (y1 < int((RECEPTIVE_FIELD_SIZE - 1)/2)):
-            start_y2 = 0
-        else:
-            start_y2 = int(y2 - (RECEPTIVE_FIELD_SIZE-1)/2)
-        #print("start:", start_y1, start_y2)
+    def localization_2(self, y1, y2):
+        shift = int((RECEPTIVE_FIELD_SIZE - 1) / 2)
+        # print("y12:", y1, y2)
+        y1_lower_bound = y1 - shift
+        y1_upper_bound = y1 + shift
+        y2_lower_bound = y2 - shift
+        y2_upper_bound = y2 + shift
+        # print("bound:", y1_lower_bound, y1_upper_bound, y2_lower_bound, y2_upper_bound)
+        if y1 < shift:  # shift = 1 if r=3; shift = 2 if r = 5
+            y1_lower_bound = 0
+        if y1 > RESOLUTION_RECEPTIVE - 1 - shift:  # 48 if r =3; 47 if r=5
+            y1_upper_bound = RESOLUTION_RECEPTIVE - 1
+        if y2 < shift:  # shift = 1 if r=3; shift = 2 if r = 5
+            y2_lower_bound = 0
+        if y2 > RESOLUTION_RECEPTIVE - 1 - shift:  # 48 if r =3; 47 if r=5
+            y2_upper_bound = RESOLUTION_RECEPTIVE - 1
 
-        for i in range(0, RECEPTIVE_FIELD_SIZE): # 0,1,2
-            location.append([start_y1 + i, start_y2 + displacement[i]])
-        return location
+        # print("bound:", y1_lower_bound, y1_upper_bound, y2_lower_bound, y2_upper_bound)
 
-    def train_step(self, y1, y2, x1, x2, displacement):
-        loc = self.localization(y1, y2, displacement)
+        loc = []
+        for i in range(y1_lower_bound, y1_upper_bound+1):
+            for j in range(y2_lower_bound, y2_upper_bound+1):
+                if self.weights_index[i][j] == 1:
+                    loc.append([i, j])
+        # print("loc:", loc)
+        return loc
+
+    def train_step(self, y1, y2, x1, x2):
+        loc = self.localization_2(y1, y2)
         x_true = [x1, x2]
-        x_pred = self.mapping(y1, y2, displacement)
+        x_pred = self.mapping(y1, y2)
         Error = []
         for i in range(0, DIM_OUTPUT):
             error = x_true[i] - x_pred[i]
             Error.append(error**2)
             # print('error:', error)
-            for j in range(0, RECEPTIVE_FIELD_SIZE):
-                self.weights[i][int(loc[j][0])][int(loc[j][1])] += LEARNING_RATE * error / RECEPTIVE_FIELD_SIZE
+            for j in range(0, len(loc)):
+                self.weights[i][loc[j][0]][loc[j][1]] += LEARNING_RATE * error / RECEPTIVE_FIELD_SIZE
                 # print(LEARNING_RATE * error / RECEPTIVE_FIELD_SIZE)
                 # weights[i][int(loc[j][0])][int(loc[j][1])] += LEARNING_RATE * error / RECEPTIVE_FIELD_SIZE
         return (Error[0] + Error[1])**0.5
 
-    def mapping(self, y1, y2, displacement):
-        # get the predict value
-        loc = self.localization(y1, y2, displacement)
+    def mapping(self, y1, y2):
+        loc = self.localization_2(y1, y2)
         x_pred = [0, 0]
-        for i in range(0, DIM_OUTPUT):
-            for j in range(0, RECEPTIVE_FIELD_SIZE):
-                # print(i, int(loc[j][0]), int(loc[j][1]))
-                x_pred[i] += self.weights[i][int(loc[j][0])][int(loc[j][1])]
+        if len(loc)!=0:
+            for i in range(0, DIM_OUTPUT):
+                for j in range(0, len(loc)):
+                    # j is index of location, i is index of row or column
+                    # print(i, int(loc[j][0]), int(loc[j][1]))
+                    x_pred[i] += self.weights[i][loc[j][0]][loc[j][1]]
                 # x_pred[i] += weights[i][int(loc[j][0])][int(loc[j][1])]
         # print(x_pred)
         return x_pred
@@ -99,77 +126,28 @@ if __name__ == '__main__':
     x1 = list(df['savePitch'])
     x2 = list(df['saveRoll'])
     # weights = np.full([2, 50, 50], 0)
+    RECEPTIVE_FIELD_SIZE = 3
     cmac = CMACNetwork()
-    # cmac.localization(y1, y2)
-    # MeanError = 0
     MSE = []
-    for epoch in range(0, TRAINING_ITERATIONS):
-        MeanError = 0
-        for i in range(0, len(y1)):
-            x1_q, x2_q, y1_q, y2_q = cmac.quantization(x1[i], x2[i], y1[i], y2[i])
-            # print("xy", x1_q, x2_q, y1_q, y2_q)
-            MeanError += cmac.train_step(y1_q, y2_q, x1_q, x2_q, DISPLACEMENT_n3)
-            cmac.mapping(y1_q, y2_q, DISPLACEMENT_n3)
-        print("epoch", epoch, "MSE", MeanError/len(y1))
-        MSE.append(MeanError/len(y1))
-    Weight = cmac.weights[0]
-    print("Weight:", Weight)
-    #ax = sns.heatmap(Weight, cmap="YlGnBu")
-    #plt.savefig("heatmap_3.png")
+    cmac.weightindex_init()
+    # ax = sns.heatmap(cmac.weights_index, cmap="YlGnBu") # visual the displacement
     # plt.show()
-    # plt.plot(MSE, 'blue', label="caseB-r3")
-
-    # ---------------- 75 training samples -------------
-    y1 = list(df['saveX'][0:75])
-    y2 = list(df['saveY'][0:75])
-    x1 = list(df['savePitch'][0:75])
-    x2 = list(df['saveRoll'][0:75])
-    MSE = []
-    cmac = CMACNetwork()
+    # cmac.localization_2(49, 49)
+    # x = cmac.de_quantization(22,33,12,54)
+    # print(x)
     for epoch in range(0, TRAINING_ITERATIONS):
         MeanError = 0
-        for i in range(0, len(y1)):
+        for i in range(0, TRAINING_SAMPLES):
             x1_q, x2_q, y1_q, y2_q = cmac.quantization(x1[i], x2[i], y1[i], y2[i])
             # print("xy", x1_q, x2_q, y1_q, y2_q)
-            MeanError += cmac.train_step(y1_q, y2_q, x1_q, x2_q, DISPLACEMENT_n3)
-            cmac.mapping(y1_q, y2_q, DISPLACEMENT_n3)
-        print("epoch", epoch, "MSE", MeanError/len(y1))
+            MeanError += cmac.train_step(y1_q, y2_q, x1_q, x2_q)
+            cmac.mapping(y1_q, y2_q)
+        print("epoch", epoch, "MSE", MeanError/TRAINING_SAMPLES)
         MSE.append(MeanError/len(y1))
-    #plt.plot(MSE, 'red', label="caseA")
-    #plt.legend(loc = "upper right")
-    #plt.title("MSE over Epochs")
-    #plt.savefig('MSEoverEpoch.png')
-    #plt.show()
-
-    # ---------------- 150 training samples n-a 5-------------
-    RECEPTIVE_FIELD_SIZE = 5
-    y1 = list(df['saveX'][0:75])
-    y2 = list(df['saveY'][0:75])
-    x1 = list(df['savePitch'][0:75])
-    x2 = list(df['saveRoll'][0:75])
-    MSE = []
-    cmac = CMACNetwork()
-    for epoch in range(0, TRAINING_ITERATIONS):
-        MeanError = 0
-
-        for i in range(0, len(y1)):
-            x1_q, x2_q, y1_q, y2_q = cmac.quantization(x1[i], x2[i], y1[i], y2[i])
-            # print("xy", x1_q, x2_q, y1_q, y2_q)
-            MeanError += cmac.train_step(y1_q, y2_q, x1_q, x2_q, DISPLACEMENT_n5)
-            cmac.mapping(y1_q, y2_q, DISPLACEMENT_n5)
-        print("epoch", epoch, "MSE", MeanError / len(y1))
-        MSE.append(MeanError / len(y1))
     Weight = cmac.weights[0]
-    print("Weight:", Weight)
+    # print("Weight:", Weight)
     ax = sns.heatmap(Weight, cmap="YlGnBu")
-    plt.savefig("heatmap_5.png")
-    #plt.plot(MSE, 'red', label="caseB-r5")
-    #plt.legend(loc="upper right")
-    #plt.title("MSE over Epochs")
-    #plt.savefig('MSEoverEpoch-2.png')
-    #plt.show()
-
-
-    # print(cmac.weights[0])
-
-
+    plt.savefig("heatmap_5(v2).png")
+    # plt.show()
+    plt.clf()
+    plt.plot(MSE, 'blue', label="caseB - r3")
