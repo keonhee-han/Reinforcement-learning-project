9deca1284872748e416c55aca47b337757375649
Keonhee Han <ge96tir@tum.de>
2022-06-25 01:18:08 +0200

    utils

---
+++ src/tutorial_4/scripts/utils.py
@@ -0,0 +1,14 @@
+class NormalizeTransform:
+    def __init__(self, mean, std):
+        """
+        :param mean: mean of images to be normalized
+            can be a single value, or a numpy array of size C
+        :param std: standard deviation of images to be normalized
+             can be a single value or a numpy array of size C
+        """
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, images):
+        images = (images - self.mean) / self.std
+        return images
