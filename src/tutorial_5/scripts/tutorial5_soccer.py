2e483ebf65c1b30b61eab22f23605c6da8683dd4
btknzn <tr5317426818@gmail.com>
2022-07-01 15:51:53 +0200

    one_foot_stand

--- src/tutorial_5/scripts/tutorial5_soccer.py
+++ src/tutorial_5/scripts/tutorial5_soccer.py
@@ -27,9 +27,9 @@ class tutorial5_soccer:
 
     # Callback function for reading in the joint values
     def joints_cb(self, data):
-        # rospy.loginfo("joint states "+str(data.name)+str(data.position))
+        #rospy.loginfo("joint states "+str(data.name)+str(data.position))
         # store current joint information in class variables
-        self.joint_names = data.name
+        self.joint_names = data.name  # LHipRoll for move in or move out
         self.joint_angles = data.position
         self.joint_velocities = data.velocity
 
@@ -58,11 +58,22 @@ class tutorial5_soccer:
         joint_angles_to_set.joint_names.append(topic) # each joint has a specific name, look into the joint_state topic or google  # When I
         joint_angles_to_set.joint_angles.append(head_angle) # the joint values have to be in the same order as the names!!
         joint_angles_to_set.relative = False # if true you can increment positions
-        joint_angles_to_set.speed = 0.1 # keep this low if you can
+        joint_angles_to_set.speed = 0.08 # keep this low if you can
+        #print(str(joint_angles_to_set))
+        self.jointPub.publish(joint_angles_to_set)
+
+    def set_joint_angles_fast(self, head_angle, topic):
+        # fast motion!! careful
+        joint_angles_to_set = JointAnglesWithSpeed()
+        joint_angles_to_set.joint_names.append(topic) # each joint has a specific name, look into the joint_state topic or google  # When I
+        joint_angles_to_set.joint_angles.append(head_angle) # the joint values have to be in the same order as the names!!
+        joint_angles_to_set.relative = False # if true you can increment positions
+        joint_angles_to_set.speed = 0.4 # keep this low if you can
         #print(str(joint_angles_to_set))
         self.jointPub.publish(joint_angles_to_set)
 
     def set_joint_angles_list(self, head_angle_list, topic_list):
+        # set the init one stand mode, doing it by all list together
         if len(head_angle_list) == len(topic_list):
             for i in range(len(topic_list)):
                 head_angle = head_angle_list[i]
@@ -74,22 +85,25 @@ class tutorial5_soccer:
                 joint_angles_to_set.speed = 0.1 # keep this low if you can
                 #print(str(joint_angles_to_set))
                 self.jointPub.publish(joint_angles_to_set)
+                rospy.sleep(0.05)
 
     # Moves its left hip back and forward and then goes back into its initial position
     def kick(self):
+        # i think
+
         self.set_stiffness(True)
         # Set ankle position to zero
-        self.set_joint_angles(0.0, "LAnklePitch")
+        #self.set_joint_angles(0.0, "LAnklePitch")
         # Set hip roll position to zero
-        self.set_joint_angles(0.0,"LHipPitch")
+        #self.set_joint_angles(0.0,"LHipPitch")
         # Move foot back
-        self.set_joint_angles(-0.1, "LHipPitch")
+        self.set_joint_angles(0.48, "LHipPitch")
         rospy.sleep(1.0)
 
-        self.set_joint_angles(-0.2, "LHipPitch")
+        self.set_joint_angles_fast(-1.1, "LHipPitch")
         # Move the foot back after kick
         rospy.sleep(2.0)
-        self.set_initial_pos()
+        self.one_foot_stand()
         #self.set_joint_angles(0.352, "LHipPitch")
 
     def set_initial_stand(self):
@@ -103,24 +117,62 @@ class tutorial5_soccer:
         print(postureProxy.getPostureFamily())
 
     def one_foot_stand(self):
+        # it is the init state ready for kicking
+        # careful !!!!!! very easy to fall
         print('one foot mode')
         self.set_stiffness(True)
-        #self.set_joint_angles(0.3, "LHipRoll")
-        position = [-0.20099592208862305, -0.10128593444824219, 1.8530300855636597, 0.16716408729553223, -1.188891887664795,
-               -0.03490658476948738, 0.6150920391082764, 0.2955999970436096, 0.052197933197021484, 0.17184996604919434,
-               0.12122797966003418, 0.23466014862060547, -0.1764519214630127, -0.08432793617248535,
-               0.052197933197021484, 0.07213997840881348, 0.2791459560394287, 0.2025299072265625, -0.3021559715270996,
-               -0.09046411514282227, 1.7457342147827148, -0.19485998153686523, 1.2118180990219116, 0.03685808181762695,
-               0.3573801517486572, 0.3255999684333801]
+        # using the rostopic echo to get the desired joint states, not perfect
+        # rostopic echo /joint_states
+
+        # way1 easy to fall when kick
+        position = [0.004559993743896484, 0.5141273736953735, 1.8330880403518677, 0.19937801361083984, -1.9574260711669922,
+                    -1.5124820470809937, -0.8882279396057129, 0.32840001583099365, -0.13955211639404297, 0.31297802925109863,
+                    -0.3911280632019043, 1.4679961204528809, -0.8943638801574707, -0.12114405632019043, -0.13955211639404297,
+                    0.3697359561920166, 0.23772811889648438, -0.09232791513204575, 0.07980990409851074, -0.3282339572906494,
+                    1.676703929901123, -0.45717406272888184, 1.1964781284332275, 0.18872404098510742, 0.36965203285217285, 0.397599995136261]
+        '''
+        # way 2
+        
+        position = [-0.015382051467895508, 0.5120565295219421, 1.8346221446990967, 0.1779019832611084, -1.937483787536621,
+                    -1.5124820470809937, -1.0692400932312012, 0.32760000228881836, -0.11807608604431152, 0.31297802925109863,
+                    -0.3911280632019043, 1.4664621353149414, -0.8943638801574707, -0.12114405632019043, -0.11807608604431152,
+                    0.3697359561920166, 0.2530679702758789, -0.09232791513204575, -0.07665801048278809, -0.269942045211792,
+                    1.6951122283935547, -0.4617760181427002, 1.1949440240859985, 0.2025299072265625, 0.3589141368865967, 0.39399999380111694]
+        '''
+        # backup init position
+        # way3 [0.004559993743896484, 0.5141273736953735, 1.8330880403518677, 0.15335798263549805, -1.9129400253295898, -1.5032780170440674, -1.199629783630371, 0.32760000228881836, -0.22852396965026855, 0.4019498825073242, -0.3911280632019043, 1.4679961204528809, -0.8943638801574707, -0.12114405632019043, -0.22852396965026855, 0.3697359561920166, 0.23772811889648438, -0.09232791513204575, 0.08594608306884766, -0.3052239418029785, 1.6905097961425781, -0.44950389862060547, 1.1995460987091064, 0.1994619369506836, 0.3512439727783203, 0.3952000141143799]
+
+        # way2 not ok [-0.015382051467895508, 0.5120565295219421, 1.8346221446990967, 0.1779019832611084, -1.937483787536621, -1.5124820470809937, -1.0692400932312012, 0.32760000228881836, -0.11807608604431152, 0.31297802925109863, -0.3911280632019043, 1.4664621353149414, -0.8943638801574707, -0.12114405632019043, -0.11807608604431152, 0.3697359561920166, 0.2530679702758789, -0.09232791513204575, -0.07665801048278809, -0.269942045211792, 1.6951122283935547, -0.4617760181427002, 1.1949440240859985, 0.2025299072265625, 0.3589141368865967, 0.39399999380111694]
         joints =['HeadYaw', 'HeadPitch', 'LShoulderPitch', 'LShoulderRoll', 'LElbowYaw', 'LElbowRoll', 'LWristYaw',
                 'LHand', 'LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 'LAnklePitch', 'LAnkleRoll', 'RHipYawPitch',
                 'RHipRoll', 'RHipPitch', 'RKneePitch', 'RAnklePitch', 'RAnkleRoll', 'RShoulderPitch', 'RShoulderRoll',
                 'RElbowYaw', 'RElbowRoll', 'RWristYaw', 'RHand']
         self.set_joint_angles_list(position, joints)
 
+    def move_in(self):
+        print('move in')
+        LHipRoll_angle = self.joint_angles[self.joint_names.index('LHipRoll')]
+        print(self.joint_names.index('LHipRoll'), LHipRoll_angle)
+        if LHipRoll_angle > 0.45:
+            increment = -0.03
+            self.set_joint_angles(LHipRoll_angle + increment, "LHipRoll")
+        else:
+            print('low bound!!!')
 
-    # fix the joints to the initial positions for the standing position
+    def move_out(self):
+        print('move out')
+        LHipRoll_angle = self.joint_angles[self.joint_names.index('LHipRoll')]
+        #print(self.joint_names.index('LHipRoll'), LHipRoll_angle)
+        if LHipRoll_angle < 0.74:
+            increment = 0.03
+            self.set_joint_angles(LHipRoll_angle + increment, "LHipRoll")
+        else:
+            print('upper bound')
+
     def set_initial_pos(self):
+        # fix the joints to the initial positions for the standing position
+        # I used the set_initial_stand instead - Tianle
+
         # Head
         self.set_stiffness(True)
         self.set_joint_angles(-0.13503408432006836, "HeadYaw")
@@ -168,7 +220,7 @@ class tutorial5_soccer:
         except rospy.ServiceException, e:
             rospy.logerr(e)
 
-
+    '''
     def touch_cb(self,data):
         if data.button == 1 and data.state == 1:  # TB1
             #self.set_stiffness(True)
@@ -185,10 +237,23 @@ class tutorial5_soccer:
         # left knee joint pitch: -0.092346 to 2.112528
         # Left hip joint pitch: -1.535889 to 0.484090
         # for RL motions the left hip roll is important: -0.379472 to 0.790477
+    '''
 
-
-
-
+    def touch_cb(self, data):
+        if data.button == 1 and data.state == 1:  # TB1
+            print("move in")
+            self.move_in()
+        if data.button == 2 and data.state == 1:
+            print("move out")
+            self.move_out()
+        if data.button == 3 and data.state == 1:
+            print("kick")
+            self.kick()
+        # try kick motion
+        #if data.button == 3 and data.state == 1:
+        # left knee joint pitch: -0.092346 to 2.112528
+        # Left hip joint pitch: -1.535889 to 0.484090
+        # for RL motions the left hip roll is important: -0.379472 to 0.790477
 
     def tutorial5_soccer_execute(self):
 
@@ -196,19 +261,20 @@ class tutorial5_soccer:
         rospy.init_node('tutorial5_soccer_node', anonymous=True)
         self.set_stiffness(True)
         self.jointPub = rospy.Publisher("joint_angles", JointAnglesWithSpeed, queue_size=10)
+
         self.set_initial_stand()
         rospy.sleep(2.0)
         self.one_foot_stand()
-
         #rospy.Subscriber("joint_states",JointAnglesWithSpeed,self.joints_cb)
         rospy.Subscriber("tactile_touch", HeadTouch, self.touch_cb)
-
+        rospy.Subscriber('joint_states', JointState, self.joints_cb)
         # start with setting the initial positions of head and right arm
 
 
         #rospy.Subscriber("/nao_robot/camera/top/camera/image_raw", Image, self.image_cb)
 
         rospy.spin()
+
 
 if __name__=='__main__':
     node_instance = tutorial5_soccer()
