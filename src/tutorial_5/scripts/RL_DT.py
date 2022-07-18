5254b1b437c975ada84fb64c22fa4c6837c7e6c7
Tianle <103313689+TaneliLab@users.noreply.github.com>
2022-07-18 11:50:06 +0200

    Update RL_DT.py
    successful RL_DT

--- src/tutorial_5/scripts/RL_DT.py
+++ src/tutorial_5/scripts/RL_DT.py
@@ -1,182 +1,158 @@
 from sklearn import tree
 import numpy as np
 import copy
-import tutorial5_soccer
 
-def RobotMotionLookUP(state, action):
-    shift = 0
-    if (action == 0):
-        shift = -1
-    elif (action == 1):
-        shift = 1
+Q_suppose = np.zeros((10, 3))
 
-    next_state = state + shift
-    if (next_state < 0 or next_state > 9):
-        return state
-    return next_state
 
 
 class RL_DT:
-    def __init__(self, current_state=5, Rmax=100, gamma=0.9, MAXSTEPS=100):
-        self.A = {'Left': 0, 'Right': 1, 'Kick': 2}  # Possible action
-        self.sM = np.zeros((10))  # set of all state
-        self.visit_number = np.zeros((10, 3))  # counting the amount of visited state
+    def __init__(self, init_state=5, gamma=0.9, MAXSTEPS=300):
+        # self.A = {'Left': 0, 'Right': 1, 'Kick': 2}  # Possible action
+        self.A = [0, 1, 2]  # 'Left': 0, 'Right': 1, 'Kick': 2
+        self.sM = []  # set of all state
+        self.visit = np.zeros((10, 3))  # counting the amount of visited state
+        self.visit_state = np.zeros(10)
+
         self.Q = np.zeros((10, 3))  # q table
-        self.Q[0][0] = -10000  # Punish going out of boundaries
-        self.Q[9][1] = -10000
-        self.current_state = current_state  # Current state of robot leg
-        self.next_state = current_state
-        self.Rmax = Rmax  # For exploration mode, giving least visited state some reward for making explotation
+        self.reward_true = np.zeros((10, 3))
         self.transitionTree = tree.DecisionTreeClassifier()
         self.rewardTree = tree.DecisionTreeClassifier()
         # self.Pm = np.zeros((10,3))
-        self.Rm = np.zeros((10, 3))
-        self.inputTree = np.zeros(2)
+        self.Rm = np.zeros((10, 3)) # reward matrix
         self.Ch = False
         self.exp = False
-        self.deltaTransition = 0
-        self.deltaReward = 0
         self.gamma = gamma
-        self.MAXSTEPS = MAXSTEPS
+        self.state_num = 10
+        self.maxstep = MAXSTEPS
+        self.init_state = init_state
+        self.possible_state = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-    def add_experience_trans(self, state, action, state_change):
-        tmp = np.append(np.array(action), np.array(state))
-        self.inputTree = np.vstack((self.inputTree, tmp))
-        self.deltaTransition = np.append(self.deltaTransition, state_change)
-        self.transitionTree = self.transitionTree.fit(self.inputTree, self.deltaTransition)
+        self.X_train = []
+        self.y_train = []
+
+
+    def reward_init(self):
+        for i in range(10):
+            for j in range(3):
+                if j == 0 or j == 1:
+                    self.reward_true[i][j] = -1
+                if j == 2:
+                    self.reward_true[i][j] = -2
+                if j == 2 and (i == 3 or i == 5):
+                    self.reward_true[i][j] = 20
+                if j == 2 and (i == 9 or i == 0):
+                    self.reward_true[i][j] = -20
+        print(self.reward_true)
+
+
+    def State_Transition(self, state, action):
+        shift = 0
+        if action == 0:
+            shift = -1
+        elif action == 1:
+            shift = 1
+        next_state = state + shift
+        if next_state < 0 or next_state > 9:
+            return state
+        return next_state
+
+
+    def get_predictions(self, s_m, a_m):
+        r_pred = self.rewardTree.predict([[s_m, a_m]])
+        return r_pred[0]
+
+
+    def add_experience_to_tree(self, s, action, r):
+        self.X_train.append([s, action])
+        self.y_train.append(r)
+        self.rewardTree.fit(self.X_train, self.y_train)
         return True
 
-    def add_experience_reward(self, reward):
-        self.deltaReward = np.append(self.deltaReward, reward)
-        self.rewardTree = self.rewardTree.fit(self.inputTree, self.deltaReward)  # must be of form samples,
-        return True
 
-    def combine_results(self, state, action):
-        prob = np.max(self.transitionTree.predict_proba([[action, state]]))
-        return prob
+    def Update_Model(self,s,action,r, s_prime):
+        # not completed
+        n = self.state_num
+        self.Ch = self.add_experience_to_tree(s, action, r)
+        for s_m in self.sM:
+            for a_m in self.A:
+                # print("pred:", s_m, a_m, self.get_predictions(s_m, a_m))
+                self.Rm[s_m][a_m] = self.get_predictions(s_m, a_m)
+        return self.Ch
+                # self.Rm[s_m][a_m] = self.reward_true[s_m][a_m]
 
-    def get_predictions(self, state, action):
-        return self.rewardTree.predict([[action, state]])
 
-    def update_model(self, action, reward):
-        # rel_state_change = self.next_state - self.current_state  # should result in -1, 0 or 1
-        # self.Ch = self.add_experience_trans(self.current_state, action, rel_state_change)
-        # self.add_experience_reward(reward)
-        # for state in range(len(self.sM)):
-        #     for action in range(len(self.A)):
-        #         # self.Pm[state, action] = self.combine_results(state, action)
-        #         prediction = self.get_predictions(state, action)
-        #         self.Rm[state][action] = prediction
+    def Check_Model(self):
+        for r in np.nditer(self.Rm):
+            if r > 0:
+                return True
+        return False
 
-        return True
 
-    def check_model(self):
-        self.exp = np.all(self.Rm[:, :] < 0)
 
-    def check_convergence(self, action_values_temp):
-        for i in range(self.Q.shape[0]):
-            for j in range(self.Q.shape[1]):
-                if (abs(self.Q[i][j] - action_values_temp[i][j]) > 0.01):
-                    return False
-        return True
-
-    def compute_values(self):
-
-        minvisit = np.min(self.visit_number)
-        converged = False
-        while not converged:
-            action_values_temp = copy.deepcopy(self.Q)
-            for s in range(0, self.Q.shape[0]):
-                for a in range(0, self.Q.shape[1]):
-                    if (s == 0 and a == 0 or s == 9 and a == 1):
-                        continue
-                    if self.exp and self.visit_number[s][a] == minvisit:
-                        self.Q[s][a] = self.Rmax
-
+    def Compute_Value_test(self, stepsize):
+        # Value iteration
+        # print("Compute_value")
+        minivisits = np.min(self.visit)
+        print("visit:", self.visit)
+        for step in range(0, stepsize):
+            for s in self.sM:
+                for a in self.A:
+                    if self.exp and self.visit[s][a] == minivisits:
+                        # print("RMax")
+                        self.Q[s][a] = 999
                     else:
-                        self.Q[s][a] = self.Rm[s, a]
-                    s_next = RobotMotionLookUP(s, a)
+                        # print("R")
+                        self.Q[s][a] = self.Rm[s][a]
+                        s_prime = self.State_Transition(s, a)
+                        self.Q[s][a] += self.gamma*max(self.Q[s_prime][:])
 
-                    self.Q[s][a] += self.gamma * np.max(self.Q[s_next][:])
-            converged = self.check_convergence(action_values_temp)
+        return 0
 
-    def execute_action(self, action):
-        self.next_state = RobotMotionLookUP(self.current_state, action)
-        # please update reward function
-        print("Your current state: " + str(self.current_state))
-        print("Next state: " + str(self.next_state))
-
-        # Hardcoded rewards for testing purposes
-        reward = -1
-        if(action==2 and (self.current_state == 0 or self.current_state == 3)):
-            reward = 20
-        return reward
+    def q_max(self, state):
+        Q = self.Q[state][:]
+        max_q = Q[0]
+        max_i = 0
+        for i in range(len(Q)):
+            if Q[i] > max_q:
+                max_q = Q[i]
+                max_i = i
+        # print("max_action:", max_i)
+        return max_i
 
 
     def execute(self):
-        Soccer = tutorial5_soccer.tutorial5_soccer
-
-        for i in range(1000):
-
+        s = self.init_state
+        self.sM.append(s)
+        # print(self.Q[s][:])
+        # self.visit_dict_state[s] = 0
+        for step in range(self.maxstep):
+            action = self.q_max(s)  # greedy action
+            print("maxaction:", action)
+            self.visit[s][action] += 1
+            s_prime = self.State_Transition(s, action)
+            # print("visit:", self.visit_dict)
+            r = self.reward_true[s][action]
+            if s_prime not in self.sM:
+                self.sM.append(s_prime)
+            self.Update_Model(s, action, r, s_prime)
+            # self.exp = self.Check_Model()
             self.exp = True
-            action = np.argmax(self.Q[self.current_state])
-            print("a:", action)
-            print("Choosing action: " + str(action))
-            Soccer.make_action()
-
-            reward = self.execute_action(action)
-
-            self.visit_number[self.current_state][action] += 1
-            print("Current visit-table: ")
-            print(self.visit_number)
-
-
-            self.Rm[self.current_state][action] = reward
-            print("Reward Tree: ")
-            print(self.Rm)
-            self.Ch = True
-
+            # print("exp:", self.exp)
+            if step>100:
+                self.exp = False
             if self.Ch:
-                self.compute_values()
-
-            print("Q-Table: ")
+                # self.Compute_Value(300)
+                self.Compute_Value_test(500)
+            s = s_prime
             print(self.Q)
-            self.current_state = self.next_state
+        print(self.sM)
+        print(self.Rm)
 
-    def ros_execute(self):
-        print("class enter")
-        print("self.current_state")
-        Soccer = tutorial5_soccer.tutorial5_soccer()
-
-        for i in range(1000):
-
-            self.exp = True
-            action = np.argmax(self.Q[self.current_state])
-            print("a:", action)
-            print("Choosing action: " + str(action))
-            Soccer.make_action(action)
-
-            reward = self.execute_action(action)
-
-            self.visit_number[self.current_state][action] += 1
-            print("Current visit-table: ")
-            print(self.visit_number)
-
-            self.Rm[self.current_state][action] = reward
-            print("Reward Tree: ")
-            print(self.Rm)
-            self.Ch = True
-
-            if self.Ch:
-                self.compute_values()
-
-            print("Q-Table: ")
-            print(self.Q)
-            self.current_state = self.next_state
-
+        return 0
 
 
 if __name__ == '__main__':
-    current_location = 5
-    RL_DT = RL_DT(current_location)
+    RL_DT = RL_DT()
+    RL_DT.reward_init()
     RL_DT.execute()
