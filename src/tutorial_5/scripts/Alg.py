70aeb132720b62f95815dba0f73c57826a8927fa
Keonhee Han <ge96tir@tum.de>
2022-07-19 02:58:25 +0200

    RL_DT_draft_v3

--- src/tutorial_5/scripts/Alg.py
+++ src/tutorial_5/scripts/Alg.py
@@ -2,15 +2,16 @@ from sklearn import tree
 import numpy as np
 
 class RL_DT_test:  # execute action at given state (as discretized state indices)
-    def __init__(self, RMax_, s_):
+    def __init__(self, RMax=15, s_= 3):
         states = range(10)  # number of states (assume discretized leg distance from the hip is 10
         self.s_ = s_
         self.train_epsiodes = 100   # random guess
         self.gamma = 0.9 # discount_factor
         self.exp_ = False
-        self.initial_state = initial_states
+        self.initial_state = s_
         ##[hkh] initialize each visit for each state zero for each state
-        self.actions = {"move_right": 0, "move_left": 1, "kick": 2}
+        # self.actions = {"move_right": 0, "move_left": 1, "kick": 2}
+        self.actions = [0, 1, 2]
         self.reward = {"move_leg": -1, "fall": -20, "fail_goal": -2, "goal": 20 }
         # self.G_t = 0 # total future reward up to given time t (for one episode)
         # self.stateSet = {s: self.actions for s in states}  # =visits: state-action pair is initially zero, no states visited so far
@@ -24,6 +25,37 @@ class RL_DT_test:  # execute action at given state (as discretized state indices
         self.leg_state_abs = 0 #-0.379472 to 0.790477 -> discretized by 10 ~ 0.117 per bin
         self.leg_state_dis = 10   # 0 - 9, 10 for invalid
 
+        ## compute value
+        self.RMax = RMax
+        self.P_M = P_M
+        self.R_M = R_M
+        self.visit_table = S_M  # stateSet
+        self.A_ = A_
+        self.exp_ = exp_
+        self.gamma = Gamma
+        self.K_ = np.asarray(S_M[:,0])
+        self.min_visits = 0
+        self.converged = False
+        self.Q_table = Q_table
+        self.MAX_STEP = MAX_STEP
+        self.min_visits = 0
+
+        ## update model
+        self.state = state
+        self.action = action
+        self.next_state = next_state
+        self.S_M = S_M  # stateSet
+        self.reward = reward
+        self.A_ = A_
+        self.transitionTree = tree.DecisionTreeClassifier()
+        self.rewardTree = tree.DecisionTreeClassifier()
+        self.inputTree = np.zeros(2)
+        self.deltaTransition = 0
+        self.deltaReward = 0
+
+        self.P_M = np.asarray(S_M)
+        self.R_M = np.asarray(S_M)
+
         ##[hkh] we have to implement DT for transition probability. DT
         # (reminder: in DT example, the state should be vaild within the possible range.
         # e.g. In A=L node, if True for x=0 meaning no movement, then its output is either 0:no movement as true, -1:moved left
@@ -32,40 +64,40 @@ class RL_DT_test:  # execute action at given state (as discretized state indices
     def run(self):
         s_next = 0
         for _ in range(self.train_epsiodes):  # end if s <- s'
-            a_ = self.opt_policy(self.s_)  # [hkh] its Utility func is determined by reward and transition func that are determined by DT
-            if a_ == 2: self.kick()
-            elif a_ == 1: self.move_in()
-            elif a_ == 0: self.move_out()
-            else: print("Nothing is given for optimal policy!") # 2. execute action a -> move_left, move_right or kick
-            # After taking an action, monitoring the state of the robot so that we can reward for that state-action pair.
-            reward_type = self.state_monitor(input_wait)  # check what happened to robot after taken the action and give'em reward
-            # 3. Upon taking an action, receives reward, observe next state
-            R_ += self.reward[reward_type] - self.reward["move_leg"]  # current reward: According to the algorithm, punish with amount -2 as it's moved.
-            # self.Q_table[s_, a_] += R_ + self.gamma * self.Q_table[s_, a_]
-            self.visit_table[s_, a_] += 1  # increase the state-action visits counter
-            if a_ == "move_left":
-                s_next = s_ - 1
-            elif a_ == "move_right":
-                s_next = s_ + 1
-            # 4. reaches a new state s' <=> observe new state -> just read in leg angle again
-            # s_new, _, CH_ = model_.update_model(s_, a_)
-            # 5. check if new state has already been visited <=> check if it's in S_M
-            # if not s_new in self.S_M:   # if not, add it to the stateSet in add_visited_states
-            #     self.S_M.add(s_new)
-            ## [hkh] new state action initializaiton is already done in constructor.
-            # 6. Update model -> many substeps
-            Alg2 = update_model(state=s_, action=a_, reward=R_, next_state=s_next, S_M=self.visit_table,A_=self.actions)
-            P_M, R_M, CH_ = Alg2()
-            # 7. Check policy, if exploration/exploitation mode
-            exp_ = self.check_model(P_M, R_M)
-            # 8. Compute values -> many substeps
-            if CH_:
-                Alg3 = compute_values(RMax_, P_M, R_M, self.visit_table, exp_)
-                Alg3()
-            # If kick happened, end an action sequence as one episode
-            if a_ == "kick": break
-            state = s_next
-            return state
+            while True:
+                a_ = self.opt_policy(self.s_)  # [hkh] its Utility func is determined by reward and transition func that are determined by DT
+                if a_ == 2: self.kick()
+                elif a_ == 1: self.move_in()
+                elif a_ == 0: self.move_out()
+                else: print("Nothing is given for optimal policy!") # 2. execute action a -> move_left, move_right or kick
+                # After taking an action, monitoring the state of the robot so that we can reward for that state-action pair.
+                reward_type = self.reward_input()  # check what happened to robot after taken the action and give'em reward
+                # 3. Upon taking an action, receives reward, observe next state
+                R_ += self.reward[reward_type] - self.reward["move_leg"]  # current reward: According to the algorithm, punish with amount -2 as it's moved.
+                # self.Q_table[s_, a_] += R_ + self.gamma * self.Q_table[s_, a_]
+                self.visit_table[self.s_, a_] += 1  # increase the state-action visits counter
+                if a_ == "move_left":
+                    s_next = s_ - 1
+                elif a_ == "move_right":
+                    s_next = s_ + 1
+                # 4. reaches a new state s' <=> observe new state -> just read in leg angle again
+                # s_new, _, CH_ = model_.update_model(s_, a_)
+                # 5. check if new state has already been visited <=> check if it's in S_M
+                # if not s_new in self.S_M:   # if not, add it to the stateSet in add_visited_states
+                #     self.S_M.add(s_new)
+                ## [hkh] new state action initializaiton is already done in constructor.
+                # 6. Update model -> many substeps
+                P_M, R_M, CH_ = self.update_model(state=s_,action=a_,reward=R_,next_state=s_next,S_M=self.visit_table,A_=self.actions)
+                # 7. Check policy, if exploration/exploitation mode
+                # exp_ = self.check_model(P_M, R_M)
+                exp_ = self.check_model(P_M, R_M)
+                # 8. Compute values -> many substeps
+                if CH_:
+                    self.compute_values(RMax, P_M, R_M, self.visit_table, exp_)
+                # If kick happened, end an action sequence as one episode
+                if a_ == "kick": break
+                self.s_ = s_next
+                return state
 
     def opt_policy(self, state): # optimal policy functio that chooses the action maximizing the reward
         action_next = np.argmax(self.Q_table[state, :])
@@ -74,7 +106,7 @@ class RL_DT_test:  # execute action at given state (as discretized state indices
     def discretize_leg(self):
         self.leg_state_dis = np.round((self.leg_state_abs - LEG_MIN) / (LEG_MAX - LEG_MIN) * 9)
 
-    def state_monitor(self):  # This is where the keyboard should input to give reward during certain time.
+    def reward_input(self):  # This is where the keyboard should input to give reward during certain time.
         print("move_leg:1, fall:2, fail_goal:3, goal: 4")
         value = input("Please input the reward type:\n")
         if value==1:reward_type="move_leg"
@@ -113,54 +145,40 @@ class RL_DT_test:  # execute action at given state (as discretized state indices
             print("check model: R_M error!")
 
 
-# Test class for the update algorithm
-class update_model:
-    def __init__(self, state, action, reward, next_state, S_M, A_):
-        self.state = state
-        self.action = action
-        self.next_state = next_state
-        self.S_M = S_M  # stateSet
-        self.reward = reward
-        self.A_ = A_
-        self.transitionTree = tree.DecisionTreeClassifier()
-        self.rewardTree = tree.DecisionTreeClassifier()
-        self.inputTree = np.zeros(2)
-        self.deltaTransition = 0
-        self.deltaReward = 0
-
-        self.P_M = np.asarray(S_M)
-        self.R_M = np.asarray(S_M)
-    '''
-    input parameters:
-    state_: current state discretized value of the distance from hip to the foot
-    action: action determined by the Q-table (among 0, 1 or 2)
-    reward: scalar value, given by us based on the result of the action
-    next_state: state reached after the action execution
-    output:
-    CH_: state, if model hs changed -> currently always true, must be changed
-    Reward and Transition probability predictions of shape (10,3)
-    '''
-    def __call__(self, *args, **kwargs):
+    # Test class for the update algorithm
+    def update_model(self, state, action, reward, next_state, S_M, A_):
+        '''
+        input parameters:
+        state_: current state discretized value of the distance from hip to the foot
+        action: action determined by the Q-table (among 0, 1 or 2)
+        reward: scalar value, given by us based on the result of the action
+        next_state: state reached after the action execution
+        output:
+        CH_: state, if model hs changed -> currently always true, must be changed
+        Reward and Transition probability predictions of shape (10,3)
+        '''
         CH_ = False
-        relative_state_change = self.next_state - self.state  # should result in -1, 0 or 1
-        CH_ = self.add_experience_trans(self.state, self.action, relative_state_change)
-        # Update the tree for reward
-        CH_ = self.add_experience_reward(self.reward)    # update each tree incrementally with input vector and desired output
+        relative_state_change = next_state - state  # should result in -1, 0 or 1
+        CH_ = self.add_experience_trans(state, action, relative_state_change)
+        # Update the tree for reward (FOR THE CASE WHEN ONE EPISODE IS DONE <=> AFTER KICKING MOTION)
+        if self.action == 2:
+            CH_ = self.add_experience_reward(reward)    # update each tree incrementally with input vector and desired output
+
         ## Tree updating is done.
         # Combine results for each tree into model
-        for s_M in self.S_M:
-            for a_M in range(len(self.A_)):
+        for s_M in S_M:
+            for a_M in range(len(A_)):
                 self.P_M[s_M, a_M] = self.combine_results(s_M, a_M)
                 self.R_M[s_M, a_M] = self.get_predictions(s_M, a_M)
-        print(self.P_M)
-        print(self.R_M)
-        return True, self.P_M, self.R_M, CH_
+        print("Test P_M: " + str(P_M))
+        print("Test R_M: " + str(R_M))
+        return CH_
 
 
     # Update the tranisition tree based on the relative change of the state
     def add_experience_trans(self, state, action, relative_state_change):
-        tmp = np.append(np.array(action), np.array(state))  # input vector
-        self.inputTree = np.vstack((self.inputTree, tmp))
+        tmp = np.append((action), (state))  # feature vector
+        self.inputTree = np.vstack((self.inputTree, tmp))   # (input vector) , (feature vector)
         self.deltaTransition = np.append(self.deltaTransition, relative_state_change)
         self.transitionTree = self.transitionTree.fit(self.inputTree, self.deltaTransition)
         return True
@@ -172,54 +190,11 @@ class update_model:
 
     # Compute probabilities of the state change
     def combine_results(self, state, action):
-        prob = np.max(self.transitionTree.predict_prob([[action, state]]))
+        prob = np.max(self.transitionTree.predict_proba([[action, state]]))
         return prob
 
     def get_predictions(self, state, action):
         return self.rewardTree.predict([[action, state]])
-
-
-class compute_values:
-    def __init__(self, RMax=20, P_M, R_M, S_M, A_, exp_, Gamma = 0.9, MAX_STEP = 10, Q_table):
-        self.RMax = RMax
-        self.P_M = P_M
-        self.R_M = R_M
-        self.visit_table = S_M  # stateSet
-        self.A_ = A_
-        self.exp_ = exp_
-        self.gamma = Gamma
-        self.K_ = np.asarray(S_M[:,0])
-        self.min_visits = 0
-        self.converged = False
-        self.Q_table = Q_table
-        self.MAX_STEP = MAX_STEP
-        self.min_visits = 0
-
-    def __call__(self, *args, **kwargs):
-        # Initialize all state's step counts
-        # self.steps_nearest_visited_state = {x: sys.maxint for x in range(9)}
-        # visits_values = []
-        # for state in range(len(self.visit_table)):
-        #     for action in range(3):
-                # if self.visit_table(state, action) > 0:
-                #     self.K_(state) = 0
-                # else:
-                #     self.K_(state) = None
-        self.min_visits = np.min(self.visit_table)
-        while not self.converged:
-            for state in range(len(self.visit_table)):
-                for action in range(3):
-                    if self.exp_ and self.visit_table[state, action] == self.min_visits:
-                        # unknown states are given exploration bonus
-                        self.Q_table[state, action] = self.RMax
-                    # elif self.K_(state) > self.MAX_STEP:
-                        # states out of reach
-                        # self.Q_table = self.RMax
-                    else:
-                        # update remaining state's action-values
-                        self.Q_table = self.R_M[state, action]
-                        state_next = self.State_Transition[state, action]
-                    self.Q_table[state, action] += self.gamma * 1 * np.max(state_next, action_next)
 
     def State_Transition(self, state, action):
         shift = 0
@@ -231,6 +206,21 @@ class compute_values:
         if next_state < 0 or next_state > 9:
             return state
         return next_state
+
+    def compute_values(self, RMax=15, P_M, R_M, S_M, A_, exp_):
+        min_visits = np.min(S_M)
+        while not self.converged:
+            for state in range(len(S_M)):
+                for action in range(len(A_)):
+                    if exp_ and (S_M[state, action] == min_visits or S_M[state, action] < MAX_STEP):
+                        # unknown states are given exploration bonus
+                        self.Q_table[state, action] += RMax
+                    else:
+                        # update remaining state's action-values
+                        self.Q_table = R_M[state, action]
+                        state_next = self.State_Transition[state, action]
+                    self.Q_table[state, action] += self.gamma * 1 * np.max(self.Q_table(state_next,:))
+
 
 
 if __name__ == '__main__':
